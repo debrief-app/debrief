442bd4c3943ab7950a197e35c4ce1b47a60a0f23
kyledemeule <kyle.demeule@goclio.com>
2014-07-14 22:09:18 -0700

    Adding vagrant stuff.

---
+++ vagrant/puppet/manifests/default.pp
@@ -0,0 +1,62 @@
+$as_vagrant   = 'sudo -u vagrant -H bash -l -c'
+$home         = '/home/vagrant'
+
+Exec {
+  path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin']
+}
+
+$ruby_version = '2.1.2'
+
+# --- Preinstall Stage ---------------------------------------------------------
+
+stage { 'preinstall':
+  before => Stage['main']
+}
+
+class apt_get_update {
+  exec { 'apt-get -y update':
+    unless => "test -e ${home}/.rvm"
+  }
+}
+class { 'apt_get_update':
+  stage => preinstall
+}
+
+# --- SQLite -------------------------------------------------------------------
+
+package { ['sqlite3', 'libsqlite3-dev']:
+  ensure => installed;
+}
+
+# --- Packages -----------------------------------------------------------------
+
+package{"curl":
+  ensure => installed
+}
+
+package{"build-essential":
+  ensure => installed
+}
+
+package{"libv8-dev":
+  ensure => installed
+}
+
+# --- Ruby ---------------------------------------------------------------------
+
+exec { 'install_rvm':
+  command => "${as_vagrant} 'curl -L https://get.rvm.io | bash -s stable'",
+  creates => "${home}/.rvm/bin/rvm",
+  require => Package['curl']
+}
+
+exec { 'install_ruby':
+  # We run the rvm executable directly because the shell function assumes an
+  # interactive environment, in particular to display messages or ask questions.
+  # The rvm executable is more suitable for automated installs.
+  #
+  # use a ruby patch level known to have a binary
+  command => "${as_vagrant} '${home}/.rvm/bin/rvm install ruby-${ruby_version} --binary --autolibs=enabled && rvm alias create default ${ruby_version}'",
+  creates => "${home}/.rvm/bin/ruby",
+  require => Exec['install_rvm']
+}
