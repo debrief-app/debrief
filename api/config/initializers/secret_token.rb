52da0939bacc529c3be03f91bf96c1977465547b
Kyle Demeule <kyle.demeule@goclio.com>
2014-07-14 18:02:42 -0700

    Switch to rails 4.1.0 and ruby 2.1.2

--- api/config/initializers/secret_token.rb
+++ api/config/initializers/secret_token.rb
@@ -16,4 +16,4 @@
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-Api::Application.config.secret_token = 'a696f0d49846d163d0c96e0341b791bd3cae6c80bd41cd6499f711d5121e24422be9eba7cb8bb7279d51789df0d4e645a95e078f39dd8695ae8bfe29cfcc3894'
+Api::Application.config.secret_token = '53301f2f74f511dd4e07b63544532c4cfd50064689f86abf8a509f01b2b8a304b86201bf7c2f2fc11579c7900e7dcd0d837959a2e597983dd7c0e10efcce187f'
