7d680ec059dd84f29374284ad52aa7fef3d89ec0
Hanson Wu <hanson.wu@goclio.com>
2014-07-14 17:59:40 -0700

    master Add client folder

---
+++ client/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name debriefApp
+ * @description
+ * # debriefApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('debriefApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
