6e2f7598d7d6af3023686e7ccd229af7b39de4aa
kyledemeule <kyle.demeule@goclio.com>
2014-08-09 18:17:00 -0700

    Fill in brief controller.

--- api/app/controllers/api/v1/briefs_controller.rb
+++ api/app/controllers/api/v1/briefs_controller.rb
@@ -6,4 +6,32 @@ class Api::V1::BriefsController < Api::V1::BaseController
   def show
     render json: current_user.briefs.find(params[:id])
   end
+
+  def create
+    brief = Brief.new(brief_params)
+    brief.user = current_user
+    brief.save
+    render json: brief
+  end
+
+  def update
+    brief = current_user.briefs.find(params[:id])
+    brief.update_attributes!(brief_params)
+    render json: brief
+  end
+
+  def destroy
+    brief = current_user.briefs.find(params[:id])
+    if brief.destroy
+        head :ok
+    else
+        head 400
+    end
+  end
+
+  private
+
+  def brief_params
+    params.require(:brief).permit(:text, :date)
+  end
 end
--- api/app/models/brief.rb
+++ api/app/models/brief.rb
@@ -1,4 +1,6 @@
 class Brief < ActiveRecord::Base
+  before_create :brief_before_create
+
   belongs_to :user
   belongs_to :team
   belongs_to :department
@@ -6,4 +8,11 @@ class Brief < ActiveRecord::Base
 
   validates :text, presence: true, length: {minimum: 1}
   validates :date, presence: true
+
+  private
+
+  def brief_before_create
+    self.team_id = self.user.team_id
+    self.department_id = self.user.department_id
+  end
 end
